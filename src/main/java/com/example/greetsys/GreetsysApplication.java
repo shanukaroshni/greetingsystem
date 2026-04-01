c2353659cbf6f0e35b27d2eb54f753bbe831dca4
shanukaroshni <shanukaroshni.27csb@licet.ac.in>
2026-04-01 08:25:56 +0530

    initial upload

---
+++ src/main/java/com/example/greetsys/GreetsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greetsys/GreetsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
